3ce12a897ba5eb06c21b6bd32c7e0fa77e912e2c
sergiojavaarq <sergio.coti@gmail.com>
2023-04-15 23:53:22 -0300

    Permissão CQRS

--- src/main/java/br/com/cotiinformatica/controllers/PedidosController.java
+++ src/main/java/br/com/cotiinformatica/controllers/PedidosController.java
@@ -1,16 +1,24 @@
 package br.com.cotiinformatica.controllers;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cotiinformatica.producers.PagamentoProducer;
 
 @RestController
 @RequestMapping("/api/pedidos")
 public class PedidosController {
 
-	@PostMapping
-	public void post() {
-		// TODO
+	@Autowired
+	private PagamentoProducer pagamentoProducer;
+
+	@GetMapping("{message}")
+	public String get(@PathVariable("message") String message) {
+		pagamentoProducer.send(message);
+		return "Dados enviados com sucesso!";
 	}
 
 }
